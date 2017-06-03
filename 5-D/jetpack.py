e13ab1508cb4b1b22cebab46b6b7349f02899c63
Ulrich Goertz <ug@geometry.de>
2017-06-03 22:19:38 +0200

    5-D: Jetpack (small optimizations)

--- 5-D/jetpack.py
+++ 5-D/jetpack.py
@@ -1,18 +1,6 @@
-from array import array
+from collections import namedtuple
 
-class Node:
-
-    def __init__(self, s, parent=None):
-        self.s = s  # the move (rise/fall) to reach this node from its parent
-                    # == '' for root node
-        self.parent = parent
-
-
-def add_move(ctr, item, new_reachable, pos, direction):
-    if lines[pos][ctr+1] != '.' or pos in new_reachable:
-        return
-    new_reachable[pos] = Node(direction, parent=node)
-
+Node = namedtuple('Node', ['s', 'pos', 'parent', 'depth'])
 
 length = int(input())
 
@@ -21,46 +9,44 @@ for i in range(10):
     lines.append(input())
 lines.reverse()  # number lowest line by 0
 
-reachable = Node('')
-leaves = {0: reachable, }  # leaves at depth ctr
+stack = []
+current = Node('', 0, None, 0)
+seen = set()  # do not visit positions twice
 
-for ctr in range(length-1):
-    new_leaves = {}  # dict  pos: node
-    for r, node in leaves.items():
-        if r < 9:
-            add_move(ctr, node, new_leaves, r+1, 1)
-        elif r == 9:
-            add_move(ctr, node, new_leaves, r, 1)
-        if r > 0:
-            add_move(ctr, node, new_leaves, r-1, 0)
-        elif r == 0:
-            add_move(ctr, node, new_leaves, r, 0)
-    leaves = new_leaves
+while current.depth < length-1:
+    rise = min(current.pos + 1, 9)
+    fall = max(current.pos - 1, 0)
+    cdp1 = current.depth + 1
+    if lines[rise][cdp1] == '.' and not (rise, cdp1) in seen:
+            stack.append(Node(1, rise, current, cdp1))
+            seen.add((rise, cdp1))
+    if lines[fall][cdp1] == '.' and not (fall, cdp1) in seen:
+            stack.append(Node(0, fall, current, cdp1))
+            seen.add((fall, current.depth+1))
+    current = stack.pop()
 
 
-for k in leaves:
-    movelist = []
-    n = leaves[k]
+movelist = []
+n = current
 
-    while n.parent:
-        movelist.append(n.s)
-        n = n.parent
+while n.parent:
+    movelist.append(n.s)
+    n = n.parent
 
-    moves = []
-    st = 0
-    c = 0
-    for i, m in enumerate(reversed(movelist)):
-        if m == 1:
-            if c == 0:
-                st = i
-            c += 1
-        elif c:  # does this end a rise?
-            moves.append('%d %d' % (st, c))
-            c = 0
-    if c:  # add final rise, if appropriate
+moves = []
+st = 0
+c = 0
+for i, m in enumerate(reversed(movelist)):
+    if m == 1:  # rising
+        if c == 0:
+            st = i
+        c += 1
+    elif c:  # does this end a rise?
         moves.append('%d %d' % (st, c))
+        c = 0
+if c:  # add final rise, if appropriate
+    moves.append('%d %d' % (st, c))
 
-    break # only need to consider one leaf
 
 print(len(moves))
 print('\n'.join(moves))
