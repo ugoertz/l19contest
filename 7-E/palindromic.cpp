fd64c4a4a41e26059d3018cc270747d98af4ff3a
Ulrich Goertz <ug@geometry.de>
2017-06-18 17:25:47 +0200

    7-E: First C++ version (also too slow).

---
+++ 7-E/palindromic.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <cstring>
+#include <utility>
+#include <map>
+#include <string>
+
+const int LIMIT = 1000000007;
+std::map<std::pair<int, int>, int> cache;
+
+unsigned long long pow2limit(int i) {
+  if (i==0) return 1;
+  return (pow2limit(i-1) * 2) % LIMIT;
+}
+
+
+int num_palindromes(char* s, int start, int end) {
+  // printf("%s %d-%d\n", s, start, end);
+  if (start >= end) return 0;
+
+  auto it = cache.find(std::make_pair(start, end));
+  if (it != cache.end()) return it->second;
+
+  unsigned long long result;
+
+  int i = start + 1;
+  for(; i < end; i++) {
+    if (s[i] != s[start]) break;
+  }
+  if (i == end) {
+    result = pow2limit(end-start) - 1;
+    // printf("%s %d-%d, %d D\n", s, start, end, result);
+  } else {
+    result = num_palindromes(s, start+1, end) + 1;
+    int pos = end - 1;
+    while (1) {
+      while (s[pos] != s[start]) pos--;
+      if (pos > start) {
+        result += num_palindromes(s, start+1, pos) + 1;
+        pos--;
+      } else break;
+    }
+  }
+
+  // printf("%s %d-%d, %d\n", s, start, end, result);
+  cache[std::make_pair(start, end)] = result % LIMIT;
+  return result % LIMIT;
+}
+
+
+int main() {
+  int N;
+  scanf("%d", &N);
+  for(int i=0; i<N; i++) {
+    cache.clear();
+    char s[2100];
+    scanf("%s", s);
+    printf("%d\n", num_palindromes(s, 0, strlen(s)));
+  }
+}
+
