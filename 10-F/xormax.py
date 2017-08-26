821fe7de0abf519716e1cbc025e5e18c33caa54f
Ulrich Goertz <ug@geometry.de>
2017-08-26 15:22:18 +0200

    10-F: Xormax

---
+++ 10-F/xormax.py
@@ -0,0 +1,22 @@
+n = int(input())
+d = {}
+
+for xx in input().split():
+    a = int(xx)
+    while a > 0:
+        f = a.bit_length()
+        try:
+            a ^= d[f]
+        except KeyError:
+            d[f] = a
+
+result = 0
+for f in range(60, 0, -1):
+    try:
+        result = max(result, result ^ d[f])
+    except KeyError:
+        pass
+
+print(result)
+
+
